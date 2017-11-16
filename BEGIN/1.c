8d69e183413deae1a8bdda3dc544a4f24105e65d
JesusZhao <zzgdata@gmail.com>
2017-11-17 00:39:14 +0800

    dormat code

--- BEGIN/1.c
+++ BEGIN/1.c
@@ -8,9 +8,10 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[])
+{
     int a, b;
     scanf("%d%d", &a, &b);
-    printf("%d", a+b);
+    printf("%d", a + b);
     return 0;
 }
